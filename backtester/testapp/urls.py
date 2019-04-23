5ca5bfc226f4be190cc90657d3f2f40e30ae68f1
Youdan <ga85pay@mytum.de>
2019-04-24 00:34:50 +0200

    Added url for processing page

--- backtester/testapp/urls.py
+++ backtester/testapp/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'testapp'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^results/$', views.results, name='results'),
+    url(r'^processing/$', views.processing, name='processing')
 ]

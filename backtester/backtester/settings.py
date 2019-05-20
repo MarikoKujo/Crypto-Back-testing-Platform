c5c6cff701163d889364122bf19d2556dd7ccf8d
Youdan <ga85pay@mytum.de>
2019-05-20 16:56:02 +0200

    Collected static and changed static path in settings. Updated asset files to latest.

--- backtester/backtester/settings.py
+++ backtester/backtester/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://storage.googleapis.com/idp_backtest_staticfiles/backtester_static/'
+STATIC_ROOT = '/backtester_static/'
 
 
 # In order to add file upload functionality
